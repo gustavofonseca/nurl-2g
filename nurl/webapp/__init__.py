863e8886e15ce3f4a54b9401a0fc7c5ba3de8ac8
Gustavo Fonseca <gustavo@gfonseca.net>
2017-05-24 11:33:48 -0300

    [spike] Build estático dos ativos da webapp.
    A idéia era de construir estaticamente os ativos e incluí-los no pacote
    de distribuição juntamente com seu manifesto (arquivo gerido pela
    webassets que contém um hash identificador da versão do bundle) para que
    a aplicação possa ser distribuída em pacotes binários e sem a
    necessidade de geração de arquivos no servidor de aplicação.

--- nurl/webapp/__init__.py
+++ nurl/webapp/__init__.py
@@ -14,7 +14,7 @@ def main(global_config, **settings):
     config.add_renderer('jsonp', JSONP(param_name='callback'))
 
     # URL patterns
-    config.add_static_view('static', 'static', cache_max_age=3600)
+    config.add_static_view('static', 'nurl.webapp:static')
     config.add_route('home', '/')
     config.add_route('analytics', '/analytics/{short_ref}')
     config.add_route('shortened', '/{short_ref}')
